d34a35475c5175211b019adac735df4240032ae5
Lucas Gugler <e12019849@student.tuwien.ac.at>
2024-04-25 11:36:48 +0000

    Resolve "Feature 1.3 UI elements"

--- dashboard/src/app/add-incident/add-incident.component.ts
+++ dashboard/src/app/add-incident/add-incident.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
@@ -12,6 +12,12 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import * as Leaflet from 'leaflet';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatMenuModule} from '@angular/material/menu';
+import { Person } from '../dto/person';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 
 @Component({
@@ -29,11 +35,24 @@ import {MatIconModule} from '@angular/material/icon';
     MatSidenavModule,
     MatListModule,
     MatTabsModule,
-    MatIconModule  ],
+    MatIconModule,
+    LeafletModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatTableModule   ],
   templateUrl: './add-incident.component.html',
   styleUrl: './add-incident.component.css'
 })
-export class AddIncidentComponent {
+export class AddIncidentComponent{
+
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  displayedColumnsPersons: string[] = ['name', 'gender', 'age', 'remove'];
+
+  map: Leaflet.Map;
+  marker: Leaflet.Marker;
+  persons: Person[] = [];
+
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
@@ -42,6 +61,43 @@ export class AddIncidentComponent {
   });
   isLinear = true;
   selectedIndex = 0;
+  selectedCategory = 'Atemnot';
+  
+  ngOnInit(): void {
+  }
+
+  options: Leaflet.MapOptions = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; OpenStreetMap contributors' })
+    ],
+    zoom: 10,
+    center: new Leaflet.LatLng(48.227747192035764, 16.40545336304577)
+  };
+
+  onMapReady(map: Leaflet.Map) {
+    this.map = map;
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 0);
+  }
+
+
+
+  selectLocation(event: any) {
+
+    if (this.marker) {
+      this.map.removeLayer(this.marker);
+    }
+    this.marker = Leaflet.marker(event.latlng, {icon: Leaflet.icon({
+      iconSize: [ 25, 41 ],
+      iconAnchor: [ 13, 41 ],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png'
+    })});
+    this.marker.addTo(this.map);
+  }
+
+
 
   constructor(private _formBuilder: FormBuilder, private router: Router) {}
 
@@ -53,6 +109,23 @@ export class AddIncidentComponent {
 
   changeIndex(index: number): void {
     this.selectedIndex = index;
+  }
+
+  changeCategory(category: string): void {
+    this.selectedCategory = category;
+  }
+
+  addPerson(): void {
+    this.persons.push({name: '', age: 0, gender: 'male'});
+    this.table.renderRows();
+  }
+
+  removePerson(person: Person): void {
+    const index = this.persons.indexOf(person);
+    if (index !== undefined && index !== -1) {
+      this.persons.splice(index, 1);
+      this.table.renderRows();
+    }
   }
 
 }
--- dashboard/src/app/edit-incident/edit-incident.component.ts
+++ dashboard/src/app/edit-incident/edit-incident.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,7 +12,12 @@ import {MatChipsModule} from '@angular/material/chips';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
-
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import * as Leaflet from 'leaflet';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatMenuModule} from '@angular/material/menu';
+import { Person } from '../dto/person';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-edit-incident',
@@ -29,7 +34,11 @@ import {MatIconModule} from '@angular/material/icon';
     MatSidenavModule,
     MatListModule,
     MatTabsModule,
-    MatIconModule],
+    MatIconModule,
+    LeafletModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatTableModule ],
   templateUrl: './edit-incident.component.html',
   styleUrl: './edit-incident.component.css'
 })
@@ -55,8 +64,69 @@ export class EditIncidentComponent {
     this.router.navigate([this.incident]);
   }
 
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  displayedColumnsPersons: string[] = ['name', 'gender', 'age', 'remove'];
+
+  map: Leaflet.Map;
+  marker: Leaflet.Marker;
+  persons: Person[] = [];
+  selectedCategory = 'Atemnot';
+  
+  ngOnInit(): void {
+  }
+
+  options: Leaflet.MapOptions = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; OpenStreetMap contributors' })
+    ],
+    zoom: 10,
+    center: new Leaflet.LatLng(48.227747192035764, 16.40545336304577)
+  };
+
+  onMapReady(map: Leaflet.Map) {
+    this.map = map;
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 0);
+  }
+
+
+
+  selectLocation(event: any) {
+
+    if (this.marker) {
+      this.map.removeLayer(this.marker);
+    }
+    this.marker = Leaflet.marker(event.latlng, {icon: Leaflet.icon({
+      iconSize: [ 25, 41 ],
+      iconAnchor: [ 13, 41 ],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png'
+    })});
+    this.marker.addTo(this.map);
+  }
+
+
   changeIndex(index: number): void {
     this.selectedIndex = index;
+  }
+
+  changeCategory(category: string): void {
+    this.selectedCategory = category;
+  }
+
+  addPerson(): void {
+    this.persons.push({name: '', age: 0, gender: 'male'});
+    this.table.renderRows();
+  }
+
+  removePerson(person: Person): void {
+    const index = this.persons.indexOf(person);
+    if (index !== undefined && index !== -1) {
+      this.persons.splice(index, 1);
+      this.table.renderRows();
+    }
   }
 
 }
